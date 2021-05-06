55938330836b0a35e7a32929f26be042351db364
Kevin Mossey <kgmossey@icloud.com>
2021-05-05 20:30:31 -0400

    Removed digitalWrite functions and replaced with direct PORT writes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 // define pins
-#define hours    2
-#define minutes  3
-#define seconds  4
+#define hours_pin         2
+#define minutes_pin       3
+#define seconds_pin       4
+#define hours             4 // pin 2 mask
+#define hours_off       251 // pin 2
+#define minutes           8 // pin 3 mask
+#define minutes_off     247 // pin 3
+#define seconds          16 // pin 4 mask
+#define seconds_off     239 // pin 4
+#define tubes_off_mask  227 // B11100011
+#define tubes_on_mask    28 // B00011100
 #define A_ones   16 // PC2
 #define B_ones   14 // PC0
 #define C_ones   13 // PB5
@@ -15,59 +23,55 @@
 #include <Wire.h>
 #include "DS1307.h"
 
+/*************************
+ * Function declarations *
+ *************************/
+void update_tube_pair (byte value, byte tube_pair);
+void cycle_digits();
+void show_ram(byte addr);
+void turn_off_tubes (byte tube_mask);
+
+/********************
+ * Global variables *
+ ********************/
 DS1307 clock; //define a object of DS1307 class
-// Create an index if binary coded decimal values to easily reference
-byte bcd[100] = { 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  // 00 01 02 03 04 05 06 07 08 09
-                 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  // 10 11 12 13 14 15 16 17 18 19
-                 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,  // 20 21 22 23 24 25 26 27 28 29
-                 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,  // 30 31 32 33 34 35 36 37 38 39
-                 64, 65, 66, 67, 68, 69, 70, 71, 72, 73,  // 40 41 42 43 44 45 46 47 48 49
-                 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,  // 50 51 52 53 54 55 56 57 58 59
-                 96, 97, 98, 99,100,101,102,103,104,105,  // 60 61 62 63 64 65 66 67 68 69
-                112,113,114,115,116,117,118,119,120,121,  // 70 71 72 73 74 75 76 77 78 79
-                128,129,130,131,132,133,134,135,136,137,  // 80 81 82 83 84 85 86 87 88 89
-                144,145,146,147,148,149,150,151,152,153}; // 90 91 92 93 94 95 96 97 98 99
-byte tubelist[6][4] = {
-  {A_ones, B_ones, C_ones, D_ones}, // seconds ones place
-  {A_tens, B_tens, C_tens, D_tens}, // seconds tens place
-  {A_ones, B_ones, C_ones, D_ones}, // minutes ones place
-  {A_tens, B_tens, C_tens, D_tens}, // minutes tens place
-  {A_ones, B_ones, C_ones, D_ones}, // hours ones place
-  {A_tens, B_tens, C_tens, D_tens}  // hours tens place
-};
-enum tubeID {
-  seconds_ones, seconds_tens, minutes_ones, minutes_tens, hours_ones, hours_tens
-};
+// Create an index of binary coded decimal values to easily reference two digit numbers
+byte bcd[100] = { 0, 128, 32, 160, 16, 144, 48, 176, 64, 192,  // 00 01 02 03 04 05 06 07 08 09
+                  8, 136, 40, 168, 24, 152, 56, 184, 72, 200,  // 10 11 12 13 14 15 16 17 18 19
+                  2, 130, 34, 162, 18, 146, 50, 178, 66, 194,  // 20 21 22 23 24 25 26 27 28 29
+                 10, 138, 42, 170, 26, 154, 58, 186, 74, 202,  // 30 31 32 33 34 35 36 37 38 39
+                  1, 129, 33, 161, 17, 145, 49, 177, 65, 193,  // 40 41 42 43 44 45 46 47 48 49
+                  9, 137, 41, 169, 25, 153, 57, 185, 73, 201,  // 50 51 52 53 54 55 56 57 58 59
+                  3, 131, 35, 163, 19, 147, 51, 179, 67, 195,  // 60 61 62 63 64 65 66 67 68 69
+                 11, 139, 43, 171, 27, 155, 59, 187, 75, 203,  // 70 71 72 73 74 75 76 77 78 79
+                  4, 132, 36, 164, 20, 148, 52, 180, 68, 196,  // 80 81 82 83 84 85 86 87 88 89
+                 12, 140, 44, 172, 28, 156, 60, 188, 76, 204}; // 90 91 92 93 94 95 96 97 98 99
+// Note: Nixies should, in general, not be turned off by cathode.  The following codes
+// disable the tube by cathode by sending an invalid code to the K155ID1 chip
+byte bcd_ones_only[10] = { 15, 143,  47, 175,  31, 159,  63, 191,  79, 207}; // -0 -1 -2 -3 -4 -5 -6 -7 -8 -9
+byte bcd_tens_only[10] = {240, 248, 242, 250, 241, 249, 243, 251, 244, 252}; // 0- 1- 2- 3- 4- 5- 6- 7- 8- 9-
+
 unsigned long inner_step_start_time;
 unsigned long outer_step_start_time;
 unsigned long current_micros;
 unsigned pulse_time = 1000;
-
-/*************************
- * Function declarations *
- *************************/
-void update_tube_pair (byte value);
-void update_tube (byte tube, byte value);
-void cycle_digits();
-void show_ram(byte addr);
-void fadebetween(byte from, byte to);
 
 /********************
  * Code starts here *
  ********************/
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A_tens,  OUTPUT);
-  pinMode(B_tens,  OUTPUT);
-  pinMode(C_tens,  OUTPUT);
-  pinMode(D_tens,  OUTPUT);
-  pinMode(A_ones,  OUTPUT);
-  pinMode(B_ones,  OUTPUT);
-  pinMode(C_ones,  OUTPUT);
-  pinMode(D_ones,  OUTPUT);
-  pinMode(hours,   OUTPUT);
-  pinMode(minutes, OUTPUT);
-  pinMode(seconds, OUTPUT);
+  pinMode(A_tens,      OUTPUT);
+  pinMode(B_tens,      OUTPUT);
+  pinMode(C_tens,      OUTPUT);
+  pinMode(D_tens,      OUTPUT);
+  pinMode(A_ones,      OUTPUT);
+  pinMode(B_ones,      OUTPUT);
+  pinMode(C_ones,      OUTPUT);
+  pinMode(D_ones,      OUTPUT);
+  pinMode(hours_pin,   OUTPUT);
+  pinMode(minutes_pin, OUTPUT);
+  pinMode(seconds_pin, OUTPUT);
   Serial.begin(9600);
   clock.begin();
   if (!clock.isStarted()) {
@@ -76,11 +80,6 @@ void setup() {
     clock.fillDayOfWeek(TUE);
     clock.setTime();
   }
-  //byte myram[56]={56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1};
-  //memcpy(clock.ram, myram, 56);
-  //for (int i = 0; i<=55;i++){Serial.println(clock.ram[i]);}
-//  Serial.println();
-  //for (int i = 0; i<=55;++i){Serial.println(clock.ram[i]);}
   cycle_digits();
 }
 
@@ -90,20 +89,22 @@ void loop() {
   clock.getTime();
   while (current_micros - outer_step_start_time < 200000)
   {
-    // update hours
-    digitalWrite(seconds, LOW);
-    update_tube_pair(bcd[clock.hour]);
-    digitalWrite(hours, HIGH);
+    // update hours //
+    // Turn off seconds (which was previously lit)
+    turn_off_tubes(seconds);
+    // Update digits, turn on the hours for the set step time
+    update_tube_pair(clock.hour, hours);
     inner_step_start_time = micros();
     current_micros = micros();
     while(current_micros - inner_step_start_time < pulse_time) {
       current_micros = micros();
     }
 
-    // update minutes
-    digitalWrite(hours, LOW);
-    update_tube_pair(bcd[clock.minute]);
-    digitalWrite(minutes, HIGH);
+    // update minutes //
+    // Turn off hours (which was previously lit)
+    turn_off_tubes(hours);
+    // Update digits, turn on the minutes for the set step time
+    update_tube_pair(clock.minute, minutes);
     inner_step_start_time = micros();
     current_micros = micros();
     while(current_micros - inner_step_start_time < pulse_time) {
@@ -111,9 +112,10 @@ void loop() {
     }
 
     // update seconds
-    digitalWrite(minutes, LOW);
-    update_tube_pair(bcd[clock.second]);
-    digitalWrite(seconds, HIGH);
+    // Turn off minutes (which was previously lit)
+    turn_off_tubes(minutes);
+    // Update digits, turn on the seconds for the set step time
+    update_tube_pair(clock.second, seconds);
     inner_step_start_time = micros();
     current_micros = micros();
     while(current_micros - inner_step_start_time < pulse_time) {
@@ -122,133 +124,45 @@ void loop() {
   }
 }
 
-void update_tube_pair (byte value){
-
-  byte a2 =   1 & value ? HIGH : LOW;
-  byte b2 =   2 & value ? HIGH : LOW;
-  byte c2 =   4 & value ? HIGH : LOW;
-  byte d2 =   8 & value ? HIGH : LOW;
-  byte a1 =  16 & value ? HIGH : LOW;
-  byte b1 =  32 & value ? HIGH : LOW;
-  byte c1 =  64 & value ? HIGH : LOW;
-  byte d1 = 128 & value ? HIGH : LOW;
-
-  digitalWrite(A_tens, a1);
-  digitalWrite(B_tens, b1);
-  digitalWrite(C_tens, c1);
-  digitalWrite(D_tens, d1);
-  digitalWrite(A_ones, a2);
-  digitalWrite(B_ones, b2);
-  digitalWrite(C_ones, c2);
-  digitalWrite(D_ones, d2);
-
+void turn_off_tubes(byte tube_mask) {
+  PORTD = PORTD & (255 - tube_mask);
+  delay(100);   // give time for tubes to discharge
 }
 
-void update_tube (byte tube, byte value)
-{
-/*
-  // Dim old digit
-  for (byte brightness = 255; brightness > 55; brightness--) {
-    analogWrite(pw, brightness);
-    delay(1);
-  }
-*/
-  // full turn off digit
-  byte a = 1 & value ? HIGH : LOW;
-  byte b = 2 & value ? HIGH : LOW;
-  byte c = 4 & value ? HIGH : LOW;
-  byte d = 8 & value ? HIGH : LOW;
-  // write new digit
-  digitalWrite(tubelist[tube][0], a);
-  digitalWrite(tubelist[tube][1], b);
-  digitalWrite(tubelist[tube][2], c);
-  digitalWrite(tubelist[tube][3], d);
-/*
-  // brighten new digit
-  for (byte brightness = 55; brightness < 255; brightness++) {
-    analogWrite(pw, brightness);
-    delay(1);
-  }
-*/
+void update_tube_pair (byte value, byte tube_pair){
 
-if (0) {fadebetween(9,0);}
-}
+  PORTB = (bcd[value] << 1) & B00111110;
+  PORTC = (bcd[value] >> 5) & B00000111;
+  PORTD = PORTD | tube_pair;
 
-
-void fadebetween(byte from, byte to) {
-
-  unsigned step_start_time;
-  unsigned current_micros;
-  byte old_brightness, new_brightness;
-/*
-  byte a1 = 1 & from ? HIGH : LOW;     byte a2 = 1 & to ? HIGH : LOW;
-  byte b1 = 2 & from ? HIGH : LOW;     byte b2 = 2 & to ? HIGH : LOW;
-  byte c1 = 4 & from ? HIGH : LOW;     byte c2 = 4 & to ? HIGH : LOW;
-  byte d1 = 8 & from ? HIGH : LOW;     byte d2 = 8 & to ? HIGH : LOW;
-*/
-  new_brightness = 0;
-  for (old_brightness = 255; old_brightness > 0; old_brightness--) {
-    step_start_time = micros();
-    current_micros = micros();
-    while(current_micros - step_start_time < 2000) {
-      update_tube (0, to);
-      analogWrite(hours, old_brightness);
-
-      current_micros++;
-      current_micros = micros();
-
-      update_tube (0, from);
-      analogWrite(hours, new_brightness);
-
-      new_brightness++;
-      current_micros = micros();
-    }
-  }
-  digitalWrite(hours, HIGH);
 }
 
 void cycle_digits() {
   byte x;
   for (byte i=0;i<10;i++)
   {
-    x = (i<<4)+i;
-    //Serial.println(x);
-    outer_step_start_time = micros();
+    x = i*10 + i;
+    // update all the tubes at once
+    turn_off_tubes(hours + minutes + seconds);
+    update_tube_pair(x, hours + minutes + seconds);
+    inner_step_start_time = micros();
     current_micros = micros();
-    while (current_micros - outer_step_start_time < 250000)
-    {
-      // update hours
-      digitalWrite(seconds, LOW);
-      update_tube_pair(x);
-      digitalWrite(hours, HIGH);
-      inner_step_start_time = micros();
+    while(current_micros - inner_step_start_time < 250000) {
       current_micros = micros();
-      while(current_micros - inner_step_start_time < pulse_time) {
-        current_micros = micros();
-      }
-
-      // update minutes
-      digitalWrite(hours, LOW);
-      update_tube_pair(x);
-      digitalWrite(minutes, HIGH);
-      inner_step_start_time = micros();
-      current_micros = micros();
-      while(current_micros - inner_step_start_time < pulse_time) {
-        current_micros = micros();
-      }
-
-      // update seconds
-      digitalWrite(minutes, LOW);
-      update_tube_pair(x);
-      digitalWrite(seconds, HIGH);
-      inner_step_start_time = micros();
-      current_micros = micros();
-      while(current_micros - inner_step_start_time < pulse_time-200) {
-        current_micros = micros();
-      }
     }
   }
-
+  for (byte i=9;i>=0;i--)
+  {
+    x = i*10 + i;
+    // update all the tubes at once
+    turn_off_tubes(hours + minutes + seconds);
+    update_tube_pair(x, hours + minutes + seconds);
+    inner_step_start_time = micros();
+    current_micros = micros();
+    while(current_micros - inner_step_start_time < 250000) {
+      current_micros = micros();
+    }
+  }
 }
 
 void show_ram(byte addr) {
@@ -262,9 +176,8 @@ void show_ram(byte addr) {
   while (current_micros - outer_step_start_time < 5000000)
   {
     // update hours
-    digitalWrite(seconds, LOW);
-    update_tube_pair(bcd[x]);
-    digitalWrite(hours, HIGH);
+    turn_off_tubes(seconds);
+    update_tube_pair(x, hours);
     inner_step_start_time = micros();
     current_micros = micros();
     while(current_micros - inner_step_start_time < pulse_time) {
@@ -272,9 +185,8 @@ void show_ram(byte addr) {
     }
 
     // update minutes
-    digitalWrite(hours, LOW);
-    update_tube_pair(bcd[y]);
-    digitalWrite(minutes, HIGH);
+    turn_off_tubes(hours);
+    update_tube_pair(y, minutes);
     inner_step_start_time = micros();
     current_micros = micros();
     while(current_micros - inner_step_start_time < pulse_time) {
@@ -282,9 +194,8 @@ void show_ram(byte addr) {
     }
 
     // update seconds
-    digitalWrite(minutes, LOW);
-    update_tube_pair(bcd[z]);
-    digitalWrite(seconds, HIGH);
+    turn_off_tubes(minutes);
+    update_tube_pair(z, seconds);
     inner_step_start_time = micros();
     current_micros = micros();
     while(current_micros - inner_step_start_time < pulse_time) {
